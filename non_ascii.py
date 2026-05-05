b0208b44d97d257c0b6bd4f24b2ec73f5811e8e5
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-04 21:53:55 -0400

    Delete non_ascii.py

--- non_ascii.py
+++
@@ -1,7 +0,0 @@
-def find_non_ascii(file_path):
-    with open(file_path, 'r', encoding='utf-8', errors='ignore') as f:
-        for line_num, line in enumerate(f, 1):
-            if not line.isascii():
-                for char_num, char in enumerate(line, 1):
-                    if not char.isascii():
-                        print(f"Non-ASCII character '{char}' found at Line {line_num}, Position {char_num}")
